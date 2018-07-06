cdda1460e22093c21859703df6c00294728c444d
Diafotismos <kang.diafotismos@gmail.com>
2018-07-06 03:30:59 -0400

    Initial commit for AlbumList component.

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 // Import a library to help create a component.
 import React from 'react';
 // The curly braces signify that you don't want the whole library, just what's within.
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 // Don't need .js if javascript file.
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
-// Create a component. A component is a "thing" that creates content that appears on 
-// the device based on the returned JSX.
-// This is a function that defines the component. Must return an object that describes
-// what it should look like on the device.
-// JSX: looks like HTML, but still Javascript.
+
+/**
+ * Create a component. A component is a "thing" that creates content that appears on 
+ * the device based on the returned JSX.
+ * 
+ * This is a function that defines the component. Must return an object that describes
+ * what it should look like on the device.
+ * 
+ * JSX: looks like HTML, but still Javascript.
+ * Note: there must always be a single top-level tag. Wrap in a View if necessary.
+ */
 const App = () => (
-    <Header headerText={'Albums'} />
+    <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
 );
 
 // Render it to the device. We have to be very specific as to when to display 
