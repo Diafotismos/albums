bca62b21201fd9a3fc9b21dc7c0832e8266883ca
Diafotismos <kang.diafotismos@gmail.com>
2018-07-05 21:16:36 -0400

    Initial commit for header component.

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+// Import statements.
+import React from 'react';
+import { Text } from 'react-native';
+
+// Component. As a rule of thumb, component name is the same as file name.
+const Header = () => (
+    <Text>Albums!</Text>
+);
+
+// Make component available to other parts of the app.
+export default Header;
